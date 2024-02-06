97244016641de5d2f57c1fe01e404eabeb754838
Victor Boudet <victor.boudet@epitech.eu>
2024-02-06 10:01:21 +0100

    [ Adding makefile and first function ]

---
+++ Makefile
@@ -0,0 +1,40 @@
+##
+## EPITECH PROJECT, 2024
+## minilibC
+## File description:
+## Makefile
+##
+
+LIBNAME = libasm.so
+
+SRC = 	strlen.s\
+
+OBJ = $(SRC:.s=.o)
+
+ASM = nasm
+CFLAGS = -fno-builtin -L. -lasm -Wl,-rpath,.
+ASMFLAGS = -f elf64
+LDFLAGS = -shared
+TEST_PROG = test_prog
+
+
+all: $(LIBNAME)
+
+$(LIBNAME): $(OBJ)
+	$(CC) $(LDFLAGS) -o $@ $^
+
+%.o: %.s
+	$(ASM) $(ASMFLAGS) -o $@ $<
+
+test: $(LIBNAME) main.c
+	$(CC) $(CFLAGS) main.c -o $(TEST_PROG)
+
+clean:
+	$(RM) $(OBJ)
+
+fclean: clean
+	$(RM) $(LIBNAME)
+
+re: fclean all
+
+.PHONY: all clean
---
+++ strlen.s
@@ -0,0 +1,23 @@
+;
+;   Epitech mini-libc 2024
+;   Boudet Victor
+;   STRLEN
+;
+
+section .text
+global strlen:function
+
+strlen:
+    mov rcx, 0 ; set rcx to 0 (rcx is considered as the counter of a loop like i)
+
+.loop:
+    cmp byte [rdi + rcx], 0 ; compare the value of the byte at the address rdi + rcx with 0
+    je .end ; if value == 0 jmp to .end
+    inc rcx ; increment by 1 the value of rcx
+    jmp .loop ; go to the begin of .loop
+
+.end:
+    mov rax, 0 ; set rax to rcx (rax is the return value of a function)
+    ret ; return to the caller
+
+section .note.GNU-stack noalloc noexec nowrite progbits
