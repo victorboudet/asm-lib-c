cfb1a1131565d7b204f8081c3ccc6f2e3b12832f
Victor Boudet <victor.boudet@epitech.eu>
2024-02-20 16:49:06 +0100

    [ ADD strcase cmp function ]

--- strcasecmp.s
+++ strcasecmp.s
@@ -8,6 +8,41 @@ section .text
 global strcasecmp:function
 
 strcasecmp:
+    mov eax, 0
+    mov rcx, 0
+    jmp .loop
+
+.incr:
+    cmp bl, 0
+    je .end
+    inc rcx
+
+.loop:
+    mov bl, [rdi + rcx]
+    mov dl, [rsi + rcx]
+    cmp bl, 65
+    jl .no_up
+    cmp bl, 90
+    jg .no_up
+    add bl, 32
+
+.no_up:
+    cmp dl, 65
+    jl .no_up1
+    cmp dl, 90
+    jg .no_up1
+    add dl, 32
+
+.no_up1:
+    cmp bl, dl
+    je .incr
+
+.diff:
+    movsx eax, bl
+    movsx ebx, dl
+    sub eax, ebx
+
+.end:
     ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
