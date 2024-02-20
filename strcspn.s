7ef51f3fe689787100f4d976ad8c32c4bb0877ae
Victor Boudet <victor.boudet@epitech.eu>
2024-02-20 20:36:18 +0100

    [ADD strcspn function ]

--- strcspn.s
+++ strcspn.s
@@ -8,6 +8,34 @@ section .text
 global strcspn:function
 
 strcspn:
+    mov rcx, 0
+    jmp .loop
+
+.incr:
+    inc rcx
+    jmp .loop
+
+.loop:
+    mov al, [rdi + rcx]
+    cmp al, 0
+    je .end
+    jmp .check
+
+.check:
+    mov rdx, 0
+
+.loop_check:
+    mov dl, [rsi + rdx]
+    cmp dl, 0
+    je .incr
+    cmp dl, al
+    je .end
+    inc rdx
+    jmp .loop_check
+
+.end:
+    mov rax, rcx
     ret
+
 
 section .note.GNU-stack noalloc noexec nowrite progbits
