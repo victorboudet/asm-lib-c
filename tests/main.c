5b77ea3761fdf812dd0ba0194e01981fbd603a0a
Victor Boudet <victor.boudet@epitech.eu>
2024-02-20 13:39:36 +0100

    [ ADD strstr test ] adding a test function in main for strstr

--- tests/main.c
+++ tests/main.c
@@ -8,14 +8,20 @@
 #include <stdio.h>
 
 size_t strlen(const char *str);
+char *strstr(const char *haystack, const char *needle);
 
 void strlen_test(void)
 {
     printf("Strlen (should be '11') : '%zu'\n", strlen("Hello World"));
 }
 
+void strstr_test(void)
+{
+    printf("Strstr (should be 'World') : '%s'\n", strstr("Hello World", "World"));
+}
+
 int main(void)
 {
-    strlen_test();
+    strstr_test();
     return 0;
 }
