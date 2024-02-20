3aa9d35110c24d1d5ffc55741aacb98a42520f26
Victor Boudet <victor.boudet@epitech.eu>
2024-02-20 19:42:56 +0100

    [ ADD strpbrk function ]

--- strpbrk.s
+++ strpbrk.s
@@ -8,6 +8,35 @@ section .text
 global strpbrk:function
 
 strpbrk:
+    mov rcx, 0
+    jmp .loop
+
+.loop:
+    mov al, [rdi + rcx]
+    cmp al, 0
+    je .ret_not_found
+    inc rcx
+    jmp .comp_set
+
+.comp_set:
+    mov r8, 0
+
+.comp:
+    mov dl, [rsi + r8]
+    cmp dl, 0
+    je .loop
+    cmp al, dl
+    je .ret_found
+    inc r8
+    jmp .comp
+
+.ret_not_found:
+    mov rax, 0
+    ret
+
+.ret_found:
+    mov rax, rdi
+    add rax, rcx
     ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
