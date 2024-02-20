e3e3786c47da0a0c0ef817fc13473529040f12ef
Victor Boudet <victor.boudet@epitech.eu>
2024-02-20 20:03:51 +0100

    [ REMOVE Useless loop ]

--- memmove.s
+++ memmove.s
@@ -8,33 +8,19 @@ section .text
 global memmove:function
 
 memmove:
-mov rcx, 0
-cmp rdi, rsi
-jle .loop
-jmp .loop_back
+    mov rcx, 0
 
 .loop:
-cmp rdx, rcx
-je .end
-mov al, [rsi + rcx]
-mov [rdi + rcx], al
-inc rcx
-jmp .loop
-
-.loop_back:
-cmp rcx, rdx
-je .end
-mov r8, rdx
-sub r8, rcx
-dec r8
-mov al, [rsi + r8]
-mov [rdi + r8], al
-inc rcx
-jmp .loop_back
+    cmp rdx, rcx
+    jge .end
+    mov al, [rsi + rcx]
+    mov [rdi + rcx], al
+    inc rcx
+    jmp .loop
 
 .end:
-mov r8, 0
-mov rax, rdi
-ret
+    mov r8, 0
+    mov rax, rdi
+    ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
